bf94fbbef30f9fab61935067d766ead1b107230b
anwersayeed <deeyasrewna@gmail.com>
2023-02-12 17:09:54 +0530

    Kafka 1 broker 1 partition

---
+++ producer.py
@@ -0,0 +1,17 @@
+import json
+import time
+from kafka import KafkaProducer
+from data import get_registered_user
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=json_serializer)
+
+if __name__ == "__main__":
+    while 1==1:
+        registered_user = get_registered_user()
+        print(registered_user)
+        producer.send("registered_user", registered_user)
+        time.sleep(4)
---
+++ readme.md
@@ -0,0 +1,4 @@
+// https://www.youtube.com/watch?v=Q4XA5nUpLeo&list=PLxoOrmZMsAWxXBF8h_TPqYJNsh3x4GyO4&index=8
+// Watch this video for setup, but as I have not installed any GUI, my work is terminal based
+
+// start zookeeper and kafka together, then run this producer.py to send data at every 4 seconds, It will going to the topic that was manually created using GUI
